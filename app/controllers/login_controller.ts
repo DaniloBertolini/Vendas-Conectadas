75015aa61dbc4cfd675a7dbe4c0ddf230241a29a
Danilo Bertolini <danilocbertolini@gmail.com>
2024-04-03 14:24:39 -0300

    Validações na Rota Login

--- app/controllers/login_controller.ts
+++ app/controllers/login_controller.ts
@@ -2,41 +2,49 @@ import type { HttpContext } from '@adonisjs/core/http'
 import User from '../models/user.js'
 import hash from '@adonisjs/core/services/hash'
 import jwt from 'jsonwebtoken'
+import { loginValidator } from '../validators/user.js'
 
 const SECRET_KEY = process.env.SECRET_KEY || 'senhasecreta'
 
 export default class LoginController {
   async signup({ request, response }: HttpContext) {
     const body = request.body()
-    body.password = await hash.make(body.password)
 
-    const user = await User.create(body)
+    try {
+      await request.validateUsing(loginValidator)
 
-    response.status(201)
+      body.password = await hash.make(body.password)
 
-    return {
-      data: user,
+      const user = await User.create(body)
+      user.password = '########'
+
+      return response.status(201).json({ data: user })
+    } catch (error) {
+      const message =
+        error.code === 'ER_DUP_ENTRY' ? 'Duplicate entry email' : error.messages[0].message
+      return { message }
     }
   }
 
   async login({ request, response }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
-    const user = await User.findBy('email', email)
 
-    if (user && (await hash.verify(user.password, password))) {
-      const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY)
+    try {
+      await request.validateUsing(loginValidator)
+      const user = await User.findBy('email', email)
 
-      response.status(200)
+      if (user) {
+        if (await hash.verify(user.password, password)) {
+          const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY)
+          return response.status(200).json({ token })
+        }
 
-      return {
-        token,
+        return response.status(401).json({ message: 'Unauthorized User' })
       }
-    }
 
-    response.status(401)
-
-    return {
-      message: 'Unauthorized User',
+      return response.status(404).json({ message: 'User does not exist' })
+    } catch (error) {
+      return response.status(422).json({ message: error.messages[0].message })
     }
   }
 }
